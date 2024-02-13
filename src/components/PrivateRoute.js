ffeb65651d3f114e3e4f192f163ddbe5b946ba4c
alex <akvashnin64@gmail.com>
2024-02-13 14:14:20 +0400

    а

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -7,27 +7,31 @@ const PrivateRoute = ({ element, ...rest }) => {
     const authToken = Cookies.get('authToken');
     console.log(authToken);
 
-  const validateToken = async (token) => {
-    try {
-      const response = await fetch('http://89.111.154.224/validate-token', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ token }),
-      });
-
-      if (response.ok) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (error) {
-      console.error('Ошибка при отправке запроса на сервер: ', error);
-      return false;
-    }
-  };
+    const validateToken = async (token) => {
+        try {
+          console.log('Отправка запроса с токеном:', token);
+      
+          const response = await fetch('http://89.111.154.224/validate-token', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ token }),
+          });
+      
+          console.log('Ответ сервера:', response);
+      
+          if (response.ok) {
+            return true;
+          } else {
+            return false;
+          }
+        } catch (error) {
+          console.error('Ошибка при отправке запроса на сервер: ', error);
+          return false;
+        }
+      };
 
   const checkTokenValidity = async () => {
     if (authToken) {
