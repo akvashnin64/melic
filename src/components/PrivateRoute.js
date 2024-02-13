558ba37e29323c51af2a8ec80134c841cfb0b48b
alex <akvashnin64@gmail.com>
2024-02-13 14:21:33 +0400

    а

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,9 +3,22 @@ import { Link, useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie';
 
 const PrivateRoute = ({ element, ...rest }) => {
-  // Получение куки с токеном синхронно
-  const authToken = Cookies.get('authToken');
-  console.log('authToken:', authToken);
+  const checkTokenValidity = async () => {
+    console.log('Проверка токена...');
+    
+    // Пытаемся получить куки
+    const authToken = Cookies.get('authToken');
+
+    if (authToken) {
+      console.log('authToken:', authToken);
+      const isValid = await validateToken(authToken.split('=')[1]);
+      console.log('Токен валиден:', isValid);
+      return isValid;
+    } else {
+      console.log('Токен отсутствует');
+      return false;
+    }
+  };
 
   const validateToken = async (token) => {
     try {
@@ -29,18 +42,6 @@ const PrivateRoute = ({ element, ...rest }) => {
       }
     } catch (error) {
       console.error('Ошибка при отправке запроса на сервер: ', error);
-      return false;
-    }
-  };
-
-  const checkTokenValidity = async () => {
-    console.log('Проверка токена...');
-    if (authToken) {
-      const isValid = await validateToken(authToken.split('=')[1]);
-      console.log('Токен валиден:', isValid);
-      return isValid;
-    } else {
-      console.log('Токен отсутствует');
       return false;
     }
   };
