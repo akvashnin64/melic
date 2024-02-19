7db2936b897a6b409b53b1b82923767605205027
alex <akvashnin64@gmail.com>
2024-02-19 15:43:05 +0400

    delete feedback

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,108 +1,7 @@
 import React, { useState, useRef } from 'react';
-import emailjs from 'emailjs-com';
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
-const Modal = ({ onClose, successMessage }) => (
-  <div className='modal-overlay' onClick={onClose}>
-    <div className='modal' onClick={(e) => e.stopPropagation()}>
-      <img src='./img/close-icon.png' onClick={onClose} alt="Close Icon" />
-      <p>{successMessage}</p>
-    </div>
-  </div>
-);
-
 function Contacts () {
-  const form = useRef();
-  const [validationErrorName, setValidationErrorName] = useState('');
-  const [validationErrorEmail, setValidationErrorEmail] = useState('');
-  const [validationErrorMessage, setValidationErrorMessage] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const validateForm = () => {
-    const name = form.current['name'].value.trim();
-    const email = form.current['email'].value.trim();
-    const message = form.current['message'].value.trim();
-
-    let isValid = true;
-
-    if (name.length < 2 || !/^[а-яА-Яa-zA-Z ]+$/.test(name)) {
-      setValidationErrorName('Имя должно содержать минимум 2 буквы и состоять только из букв');
-      isValid = false;
-    } else {
-      setValidationErrorName('');
-    }
-
-    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
-      setValidationErrorEmail('Неправильный формат email');
-      isValid = false;
-    } else {
-      setValidationErrorEmail('');
-    }
-
-    if (message.length < 10 || !/^[а-яА-Яa-zA-Z0-9.,\-?!()\s]+$/.test(message)) {
-      setValidationErrorMessage('Сообщение должно содержать минимум 10 символов и состоять только из букв, цифр и знаков препинания');
-      isValid = false;
-    } else {
-      setValidationErrorMessage('');
-    }
-
-    return isValid;
-  };
-
-  const handleNameBlur = () => {
-    const name = form.current['name'].value.trim();
-    if (name.length < 2 || !/^[а-яА-Яa-zA-Z ]+$/.test(name)) {
-      setValidationErrorName('Имя должно содержать минимум 2 буквы и состоять только из букв');
-    } else {
-      setValidationErrorName('');
-    }
-  };
-  
-  const handleEmailBlur = () => {
-    const email = form.current['email'].value.trim();
-    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
-      setValidationErrorEmail('Неправильный формат email');
-    } else {
-      setValidationErrorEmail('');
-    }
-  };
-  
-  const handleMessageBlur = () => {
-    const message = form.current['message'].value.trim();
-    if (message.length < 10 || !/^[а-яА-Яa-zA-Z0-9.,?!]+$/.test(message)) {
-      setValidationErrorMessage('Сообщение должно содержать минимум 10 букв и состоять только из букв');
-    } else {
-      setValidationErrorMessage('');
-    }
-  };
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    if (!validateForm()) {
-      return;
-    }
-
-    emailjs.sendForm('service_fisgh5e', 'template_aicjr0n', form.current, 'SBC5ENSTTk7AMMeNE')
-      .then((result) => {
-        setSuccessMessage('Ваше обращение успешно отправлено, мы постараемся дать ответ как можно скорее!');
-        setValidationErrorName('');
-        setValidationErrorEmail('');
-        setValidationErrorMessage('');
-        form.current.reset();
-        setIsModalOpen(true);
-      })
-      .catch((error) => {
-        setSuccessMessage('');
-        setIsModalOpen(true);
-      });
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
     return(
         <div className='containerContactsPage'>
             <div className='infoContactsPage'>
@@ -115,14 +14,14 @@ function Contacts () {
                 </div>
                 <div className='timeContacts'>
                     <img src='/img/timeContacts.png'/>
-                    <p>ПН-ЧТ с 8:00 до 17:00<br></br>ПТ с 8:00 до 16:00</p>
+                    <p>ПН-ЧТ с 8:00 до 17:00<br></br>ПТ с 8:00 до 16:00<br></br>Перерыв с 12:00 до 12:48</p>
                 </div>
                 <div className='locationContacts'>
                     <img src='/img/locationContacts.png'/>
                     <p>410028 Саратов <br></br> ул. им. Н. Г. Чернышевского, д. 116А</p>
                 </div>
-
-                <div className="containerMapContactsPage">
+            </div>
+            <div className="containerMapContactsPage">
                     <YMaps>
                         <Map 
                         className="mapContactsPage"
@@ -131,54 +30,6 @@ function Contacts () {
                         </Map>
                     </YMaps>
                 </div>
-            </div>
-
-            <div className='feedback'>
-                <div className='headerFeedback'>
-                    <p>ОСТАВИТЬ ОБРАЩЕНИЕ</p>
-                </div>
-                <div className='textFeedback'>
-                    <p>Оставьте свои контакты и мы свяжемся с вами <span className='boldTextAbout2'>в ближайшее время</span></p>
-                </div>
-                <form className='inputFieldsFeedback' ref={form} onSubmit={sendEmail}>
-                    
-                    <input 
-                    id='nameFeedback' 
-                    name='name'
-                    className={`inputFieldFeedback defaultBorder ${validationErrorName ? 'errorField' : ''}`}
-                    type='text'
-                    placeholder='Имя *'
-                    onBlur={handleNameBlur}>
-                    </input>
-                    
-                    <input 
-                    id='emailFeedback' 
-                    name='email'
-                    className={`inputFieldFeedback defaultBorder ${validationErrorEmail ? 'errorField' : ''}`}
-                    type='email'
-                    placeholder='Email *'
-                    onBlur={handleEmailBlur}>
-                    </input>
-                    
-                    <textarea 
-                    id='message' 
-                    name='message'
-                    className={`inputFieldFeedback defaultBorder ${validationErrorMessage ? 'errorField' : ''}`}
-                    type='text'
-                    placeholder='Текст сообщения *'
-                    onBlur={handleMessageBlur}>
-                    </textarea>
-
-                    <div className='sendFeedback'>
-                        <button type='submit'>Отправить обращение</button>
-                    </div>
-                </form>
-
-                {/* Модальное окно */}
-                {isModalOpen && (
-                  <Modal onClose={() => setIsModalOpen(false)} successMessage={successMessage} />
-                )}
-              </div>
           </div>
         
     )}
