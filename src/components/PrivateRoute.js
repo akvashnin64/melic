e2fe9ca62a94caee8e14819c9531385b33b39765
alex <akvashnin64@gmail.com>
2024-04-08 12:53:58 +0400

    temporary changes

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,59 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import React from 'react';
+import { Navigate, Routes } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from 'react-cookie';
+import AdminPage from '../pages/AdminPage';
+
 
 const PrivateRoute = ({ element: Element, ...rest }) => {
     const [cookies] = useCookies(['authToken']);
     const authToken = cookies.authToken;
-    const [isValidToken, setIsValidToken] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+  
+    const checkTokenValidity = async () => {
+      console.log('Проверка токена...');
+  
+      if (authToken) {
+        console.log('authToken:', authToken);
+        const isValid = await validateToken(authToken);
+        console.log('Токен валиден:', isValid);
+        return isValid;
+      } else {
+        console.log('Токен отсутствует');
+        return false;
+      }
+    };
+  
+    const validateToken = async (token) => {
+      try {
+        console.log('Отправка запроса с токеном:', token);
+  
+        const response = await fetch('http://89.111.154.224/validate-token', {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ token }),
+        });
+  
+        console.log('Ответ сервера:', response);
+  
+        if (response.ok) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        console.error('Ошибка при отправке запроса на сервер: ', error);
+        return false;
+      }
+    };
+  
+    return checkTokenValidity() !== undefined ? <Element {...rest} /> : <Navigate to="/admin/login" replace />;
 
-    useEffect(() => {
-        const checkTokenValidity = async () => {
-            console.log('Проверка токена...');
-
-            if (authToken) {
-                console.log('authToken:', authToken);
-                try {
-                    console.log('Отправка запроса с токеном:', authToken);
-
-                    const response = await fetch('http://89.111.154.224/validate-token', {
-                        method: 'POST',
-                        credentials: 'include',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ token: authToken }),
-                    });
-
-                    console.log('Ответ сервера:', response);
-
-                    if (response.ok) {
-                        setIsValidToken(true);
-                    } else {
-                        setIsValidToken(false);
-                    }
-                } catch (error) {
-                    console.error('Ошибка при отправке запроса на сервер: ', error);
-                    setIsValidToken(false);
-                } finally {
-                    setIsLoading(false);
-                }
-            } else {
-                console.log('Токен отсутствует');
-                setIsValidToken(false);
-                setIsLoading(false);
-            }
-        };
-
-        checkTokenValidity();
-    }, [authToken]);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    return isValidToken ? <Element {...rest} /> : <Navigate to="/admin/login" replace />;
-};
-
-export default PrivateRoute;
+  };
+  
+  export default PrivateRoute;
